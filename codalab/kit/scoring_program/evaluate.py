56129d9ec5e9c488aaedaa05fb3fc0f3499e5762
Arman Cohan <armancohan@gmail.com>
2022-06-28 18:38:45 +0000

    black

--- codalab/kit/scoring_program/evaluate.py
+++ codalab/kit/scoring_program/evaluate.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import os.path
 import sys
+
 import numpy as np
 
 try:
     # installing dependencies
     os.system("pip install rouge-score")
     os.system("pip install pandas")
-    #os.system("pip install bert-score")
+    # os.system("pip install bert-score")
 except Exception as e:
     print("Error occurred while installing dependencies ", e)
     exit()
@@ -15,18 +16,18 @@ except Exception as e:
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 
-submit_dir = os.path.join(input_dir, 'res')
-truth_dir = os.path.join(input_dir, 'ref')
+submit_dir = os.path.join(input_dir, "res")
+truth_dir = os.path.join(input_dir, "ref")
 
 if not os.path.isdir(submit_dir):
     print(f"{submit_dir} doesn't exist")
 
 
 def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=False):
-    from rouge_score import rouge_scorer
     import pandas as pd
+    from rouge_score import rouge_scorer
 
-    metrics = ['rouge1', 'rouge2', 'rougeL']
+    metrics = ["rouge1", "rouge2", "rougeL"]
     ground_truth_df = pd.read_csv(test_annotation_file)
     print(f"processing ground truth file + {test_annotation_file}")
     submission_df = pd.read_csv(user_submission_file)
@@ -34,15 +35,17 @@ def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=F
 
     scorer = rouge_scorer.RougeScorer(metrics, use_stemmer=True)
     results = {"rouge1_f": [], "rouge1_r": [], "rouge2_f": [], "rouge2_r": [], "rougeL_f": [], "rougeL_r": []}
-    if len(ground_truth_df['paper_id'].unique()) == len(submission_df['paper_id'].unique()):
-        print("Warning, number of unique 'paper_id's in submission is not equal to number of unique 'paper_id's in "
-              "ground truth")
+    if len(ground_truth_df["paper_id"].unique()) == len(submission_df["paper_id"].unique()):
+        print(
+            "Warning, number of unique 'paper_id's in submission is not equal to number of unique 'paper_id's in "
+            "ground truth"
+        )
 
     bertscore_summaries = ([], [])
     for index, ground_truth_row in ground_truth_df.iterrows():
-        ground_truth_summary = ground_truth_row['summary']
-        article_id = ground_truth_row['paper_id']
-        submission_summary_row = submission_df.loc[submission_df['paper_id'] == article_id]
+        ground_truth_summary = ground_truth_row["summary"]
+        article_id = ground_truth_row["paper_id"]
+        submission_summary_row = submission_df.loc[submission_df["paper_id"] == article_id]
         if submission_summary_row.empty:
             print(f"paper with id '{article_id}' wasn't found in submission")
             raise Exception(f"paper with id '{article_id}' wasn't found in submission")
@@ -50,32 +53,34 @@ def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=F
             print(f"More than one summary submission for paper with id '{article_id}'")
             raise Exception(f"More than one summary submission for paper with id '{article_id}'")
 
-        submission_summary = submission_summary_row.iloc[0]['summary']
+        submission_summary = submission_summary_row.iloc[0]["summary"]
 
         print(f"evaluating summary for article with id '{article_id}'")
         scores = scorer.score(ground_truth_summary.strip(), submission_summary.strip())
-        
+
         for rouge_metric in metrics:
             results[rouge_metric + "_f"].append(scores[rouge_metric].fmeasure)
             results[rouge_metric + "_r"].append(scores[rouge_metric].recall)
 
         if use_bertscore:
             from bert_score import score as bert_score_func
+
             bertscore_summaries[0].append(submission_summary.strip())
             bertscore_summaries[1].append(ground_truth_summary.strip())
 
-    metrics_scores = {rouge_metric: np.average(rouge_metric_scores)
-                      for (rouge_metric, rouge_metric_scores) in results.items()}
-    avg_score = [metric_score for metric_name, metric_score in metrics_scores.items() if '_f' in metric_name]
+    metrics_scores = {
+        rouge_metric: np.average(rouge_metric_scores) for (rouge_metric, rouge_metric_scores) in results.items()
+    }
+    avg_score = [metric_score for metric_name, metric_score in metrics_scores.items() if "_f" in metric_name]
 
     if use_bertscore:
         (P, R, F) = bert_score_func(cands=bertscore_summaries[0], refs=bertscore_summaries[1], lang="en")
-        metrics_scores['BERTScore_P'] = P.mean().item()
-        metrics_scores['BERTScore_R'] = R.mean().item()
-        metrics_scores['BERTScore_F'] = F.mean().item()
+        metrics_scores["BERTScore_P"] = P.mean().item()
+        metrics_scores["BERTScore_R"] = R.mean().item()
+        metrics_scores["BERTScore_F"] = F.mean().item()
         avg_score.append(F.mean().item())
 
-    metrics_scores['Metrics_Avg'] = sum(avg_score) / len(avg_score)
+    metrics_scores["Metrics_Avg"] = sum(avg_score) / len(avg_score)
     return metrics_scores
 
 
@@ -83,14 +88,18 @@ if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    output_filename = os.path.join(output_dir, 'scores.txt')
-    output_file = open(output_filename, 'w')
+    output_filename = os.path.join(output_dir, "scores.txt")
+    output_file = open(output_filename, "w")
 
     truth_file = os.path.join(truth_dir, "testing.csv")
     submission_answer_file = os.path.join(submit_dir, "testing.csv")
     if not os.path.exists(submission_answer_file):
-        print(f"Submission file with name 'testing.csv' doesn't exist, please make sure to submit a single zip file that contains 'testing.csv'")
-        raise Exception(f"Submission file with name 'testing.csv' doesn't exist, please make sure to submit a single zip file that contains 'testing.csv'")
+        print(
+            f"Submission file with name 'testing.csv' doesn't exist, please make sure to submit a single zip file that contains 'testing.csv'"
+        )
+        raise Exception(
+            f"Submission file with name 'testing.csv' doesn't exist, please make sure to submit a single zip file that contains 'testing.csv'"
+        )
 
     eval_scores = evaluate_metrics(test_annotation_file=truth_file, user_submission_file=submission_answer_file)
     for metric, metric_score in eval_scores.items():
