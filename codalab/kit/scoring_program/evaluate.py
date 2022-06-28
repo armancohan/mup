4caf8cd9a70efbb190e0b49af7436200af07758c
Arman Cohan <armancohan@gmail.com>
2022-06-28 18:39:32 +0000

    changes

--- codalab/kit/scoring_program/evaluate.py
+++ codalab/kit/scoring_program/evaluate.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python
+import argparse
 import os.path
 import sys
 
 import numpy as np
+from tqdm import tqdm
+
+
+def install_dependencies():
+    try:
+        os.system("pip install rouge-score")
+        os.system("pip install pandas")
+    except Exception as e:
+        print(e)
+        print("Error occurred while installing dependencies. Please install manually.")
+        sys.exit(1)
+
 
 try:
     # installing dependencies
-    os.system("pip install rouge-score")
-    os.system("pip install pandas")
+    import pandas as pd
+    from rouge_score import rouge_scorer
+
     # os.system("pip install bert-score")
 except Exception as e:
-    print("Error occurred while installing dependencies ", e)
-    exit()
+    install_dependencies()
 
-input_dir = sys.argv[1]
-output_dir = sys.argv[2]
+# input_dir = sys.argv[1]
+# output_dir = sys.argv[2]
 
-submit_dir = os.path.join(input_dir, "res")
-truth_dir = os.path.join(input_dir, "ref")
+# submit_dir = os.path.join(input_dir, "res")
+# truth_dir = os.path.join(input_dir, "ref")
 
-if not os.path.isdir(submit_dir):
-    print(f"{submit_dir} doesn't exist")
+# if not os.path.isdir(submit_dir):
+#     print(f"{submit_dir} doesn't exist")
 
 
 def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=False):
-    import pandas as pd
-    from rouge_score import rouge_scorer
 
     metrics = ["rouge1", "rouge2", "rougeL"]
     ground_truth_df = pd.read_csv(test_annotation_file)
@@ -35,14 +46,16 @@ def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=F
 
     scorer = rouge_scorer.RougeScorer(metrics, use_stemmer=True)
     results = {"rouge1_f": [], "rouge1_r": [], "rouge2_f": [], "rouge2_r": [], "rougeL_f": [], "rougeL_r": []}
-    if len(ground_truth_df["paper_id"].unique()) == len(submission_df["paper_id"].unique()):
+    if len(ground_truth_df["paper_id"].unique()) != len(submission_df["paper_id"].unique()):
         print(
             "Warning, number of unique 'paper_id's in submission is not equal to number of unique 'paper_id's in "
             "ground truth"
         )
 
     bertscore_summaries = ([], [])
-    for index, ground_truth_row in ground_truth_df.iterrows():
+    for index, ground_truth_row in tqdm(
+        ground_truth_df.iterrows(), total=ground_truth_df.shape[0], desc="evaluating summaries..."
+    ):
         ground_truth_summary = ground_truth_row["summary"]
         article_id = ground_truth_row["paper_id"]
         submission_summary_row = submission_df.loc[submission_df["paper_id"] == article_id]
@@ -55,7 +68,7 @@ def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=F
 
         submission_summary = submission_summary_row.iloc[0]["summary"]
 
-        print(f"evaluating summary for article with id '{article_id}'")
+        # print(f"evaluating summary for article with id '{article_id}'")
         scores = scorer.score(ground_truth_summary.strip(), submission_summary.strip())
 
         for rouge_metric in metrics:
@@ -84,15 +97,20 @@ def evaluate_metrics(test_annotation_file, user_submission_file, use_bertscore=F
     return metrics_scores
 
 
-if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("submission_file", help="path to submission file")
+    parser.add_argument("ground_truth", help="path to ground truth file")
+    parser.add_argument("--output_dir", help="path to output directory")
+    args = parser.parse_args()
 
-    output_filename = os.path.join(output_dir, "scores.txt")
-    output_file = open(output_filename, "w")
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
-    truth_file = os.path.join(truth_dir, "testing.csv")
-    submission_answer_file = os.path.join(submit_dir, "testing.csv")
+    output_filename = os.path.join(args.output_dir, "scores.txt")
+
+    truth_file = args.ground_truth
+    submission_answer_file = args.submission_file
     if not os.path.exists(submission_answer_file):
         print(
             f"Submission file with name 'testing.csv' doesn't exist, please make sure to submit a single zip file that contains 'testing.csv'"
@@ -102,6 +120,11 @@ if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
         )
 
     eval_scores = evaluate_metrics(test_annotation_file=truth_file, user_submission_file=submission_answer_file)
-    for metric, metric_score in eval_scores.items():
-        output_file.write(f"{metric}:{(metric_score * 100):.2f}\n")
+    with open(output_filename, "w") as output_file:
+        for metric, metric_score in eval_scores.items():
+            output_file.write(f"{metric}:{(metric_score * 100):.2f}\n")
     output_file.close()
+
+
+if __name__ == "__main__":
+    main()
